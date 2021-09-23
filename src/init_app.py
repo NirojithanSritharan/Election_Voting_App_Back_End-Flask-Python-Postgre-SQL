f84950a7583800cf2127a50915c162ba3e13ed1b
Nirojithan Sritharan <49116494+NirojithanSritharan@users.noreply.github.com>
2021-09-24 01:10:22 +0530

    Updated the database URI
    User name. Password, Database name, Host

--- src/init_app.py
+++ src/init_app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 # set configs of the app instance
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Skaniroma@localhost/Election_Votes'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<User_name>:<Password>@<Host>/<Database_name>'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # instatiate database object
