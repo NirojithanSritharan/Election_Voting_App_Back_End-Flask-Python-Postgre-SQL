5fa2b16a948236519552e9a88577bdb00c86c0a5
Nirojithan Sritharan <49116494+NirojithanSritharan@users.noreply.github.com>
2021-09-24 13:45:39 +0530

    Update init_app.py
    Add database_url variable

--- src/init_app.py
+++ src/init_app.py
@@ -1,3 +1,5 @@
+# src\init_app
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -5,9 +7,12 @@ from flask_marshmallow import Marshmallow
 # instantiate flask app
 app = Flask(__name__)
 
+# Postgresql database url
+database_url = 'postgresql://<--Username-->:<--Password-->@<--Host-->/<--Database name-->'
+
 # set configs of the app instance
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://<User_name>:<Password>@<Host>/<Database_name>'
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # instatiate database object
