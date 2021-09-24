7e931adc9dc86d93fd83cb22edca16d5d1a5b99b
Nirojithan Sritharan <49116494+NirojithanSritharan@users.noreply.github.com>
2021-09-24 12:43:41 +0530

    Update app.py
    updated variables in app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import request, jsonify, make_response, json, abort
+# app.py (main file)
+
+#import required libraries
+from flask import request, jsonify, make_response, json
 from sqlalchemy.exc import OperationalError
 from werkzeug.exceptions import HTTPException
 
@@ -6,7 +9,6 @@ from src.init_app import app, db
 from src.models import VoteCount, VoteCountSchema
 
 # create instance of schemas
-voteCountSchemas = VoteCountSchema(many = True)
 voteCountSchema = VoteCountSchema(many = False)
 
 @app.errorhandler(HTTPException)
@@ -47,10 +49,13 @@ def add_vote():
         if ex_candidate is None:
             # create a new candidate
             ex_candidate = VoteCount(candidate = candidate, votes = votes)
+            print(ex_candidate.candidate)
             db.session.add(ex_candidate)
+            db.session.commit()
             
         else:
             # update existing candidate vote
+            print(ex_candidate.candidate)
             ex_candidate.votes += 1
             db.session.commit()
 
@@ -63,61 +68,33 @@ def add_vote():
             "description" : "The given keyword was not found. Please check and try again."
         }))
 
-    # except sqlalchemy.exc.OperationalError as exc:
-    #     logging.exception("METRICS: error while gathering metrics")
-    #     stats = {
-    #         "health": {
-    #         "health": 0,
-    #         "diagnosis": "Database error: {}".format(str(exc)),
-    #             }
-    #         }
-    #     return stats 
-    # except Exception as e:
-    #     response = e.get_response()
-
-    #     response.data = json.dumps({
-    #         "code" : e.code,
-    #         "name" : e.name,
-    #         "description" : e.description
-    #     })
-
-    #     response.content_type = "application/json"
-
-        # return str(e)
-
 # get candidate vote count
 @app.route('/api/candidate/<int:candidate_id>/count', methods = ['GET'])
 
 # Get the candidate deails with the given candidate_id 
 def get_vote_count(candidate_id):
 
-    # try:
-        # candidate = VoteCount.query.get_or_404(int(candidate_id))
-        
-        candidate = VoteCount.query \
-        .filter(VoteCount.id == candidate_id) \
-        .one_or_none()
+    candidate = VoteCount.query \
+    .filter(VoteCount.id == candidate_id) \
+    .one_or_none()
 
-        # Did we find a person?
-        if candidate is not None:
+    # Did we find a person?
+    if candidate is not None:
 
-            # Serialize the data for the response
-            data = voteCountSchema.dump(candidate)
+        # Serialize the data for the response
+        data = voteCountSchema.dump(candidate)
 
-            app.logger.info(candidate.votes)
+        app.logger.info(candidate.votes)
 
-            return {"votes" : candidate.votes}
+        return {"votes" : candidate.votes}
 
-        else:
-            return make_response(jsonify({
-            "status code" : "404",
-            "error" : "Not Found",
-            "description" : "Candidate not found for Id: {candidate_id}"
+    else:
+        return make_response(jsonify({
+        "status code" : "404",
+        "error" : "Not Found",
+        "description" : "Candidate not found for Id: {candidate_id}"
         }))
 
-    # except Exception as e:
-    #     print(e)
-    #     return {"error" : str(e)}
 
 if __name__ == '__main__':
     app.debug = True
